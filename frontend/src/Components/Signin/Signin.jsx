64a32f70cbe8b074ac80e6117f7efd0750b4d9c1
unknown <jyothiprakashpatnaik@gmail.com>
2022-04-01 12:07:15 +0530

    fix build issue

--- frontend/src/Components/Signin/Signin.jsx
+++ frontend/src/Components/Signin/Signin.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import {firebase} from "../../backend/firebase.config"
 import {ReactComponent as Login} from "../../assets/login.svg"
 import './Signin.styles.scss';
+
+import gicon from "../../assets/GIcon.png"
+
 function Signin() {
     const SiginiWithFirebase =() =>{
         const google_provider = new firebase.auth.GoogleAuthProvider()
@@ -18,8 +21,18 @@ function Signin() {
         <div className='sign-in'>
         <Login/>
         <button type="button"  className="login-with-google-btn" onClick = {SiginiWithFirebase}>
-        Sign in with Google
+            <table>
+                <tr>
+                    <td className='GIconTD'><img className="GIcon" src={gicon} alt="google G" /></td>
+                    <td>
+                    
+                        Sign in with Google
+                    
+                    </td>
+                </tr>
+            </table>
         </button>
+        
        
     </div>  
  
